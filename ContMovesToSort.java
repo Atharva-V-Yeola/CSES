63afece7580587ab085bb10efdc23d858d2a8031
Atharva Yeola <atharvavy24@gmail.com>
2025-05-05 18:22:41 +0530

    second day

---
+++ ContMovesToSort.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ContMovesToSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+    }
+    static int Cnt(int[] arr, int n) {
+        int ans = 0;
+        
+        return ans;
+    }
+}
